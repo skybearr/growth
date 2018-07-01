d23dd49bc8d904c253229a63a0f976e40dcc872a
skybearr <40344680@qq.com>
2018-07-01 12:15:00 +0800

    1

---
+++ growth/src/sky/model/ConstData.ts
@@ -0,0 +1,24 @@
+class ConstData {
+	public constructor() {
+	}
+
+
+}
+
+enum SKILLTYPE {
+	SKILLTYPE_NONE	= 0,
+	SKILLTYPE_CHN	= 1,			//语文
+	SKILLTYPE_MATH	= 2,			//数学
+	SKILLTYPE_ENG	= 3,			//英语
+	// SKILLTYPE_PHYSICS	= 4,			//物理
+	// SKILLTYPE_CHEMISTRY	= 5,			//化学
+	SKILLTYPE_BASE	= 11,			//基础
+	SKILLTYPE_LEARN	= 12,		//学习能力
+	SKILLTYPE_ART	= 13,			//艺术
+	SKILLTYPE_LIFE	= 14,			//生活自理能力
+	SKILLTYPE_HEALTH = 15,		//健康
+	SKILLTYPE_SOCIAL = 16,		//社交
+	SKILLTYPE_CULTURE = 17,		//文化
+	SKILLTYPE_MORAL = 18,		//品德
+	SKILLTYPE_ASPIRATION = 19,	//理想
+};
